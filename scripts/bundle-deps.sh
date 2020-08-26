952ce8c949fdc357b6e1d37ecc938496c5d24a87
Carlos Precioso <kalprestito@gmail.com>
2020-08-26 21:35:58 +0200

    no sudo

--- scripts/bundle-deps.sh
+++ scripts/bundle-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo yum install cairo-devel libjpeg-turbo-devel giflib-devel pango-devel -y
+yum install cairo-devel libjpeg-turbo-devel giflib-devel pango-devel -y
 cp /usr/lib64/libuuid.so.1 node_modules/canvas/build/Release/
