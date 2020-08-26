ec3405d9a4d7932cd93f0b2b63ed02b8a9b7bd7d
Carlos Precioso <kalprestito@gmail.com>
2020-08-26 21:32:58 +0200

    uno

---
+++ scripts/bundle-deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo yum install cairo-devel libjpeg-turbo-devel giflib-devel pango-devel -y
+# cp /usr/lib64/{libpng12.so.0,libjpeg.so.62,libpixman-1.so.0,libfreetype.so.6,libcairo.so.2,libpango-1.0.so.0,libpangocairo-1.0.so.0,libpangoft2-1.0.so.0} node_modules/canvas/build/Release/
--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,3 +1,18 @@
-module.exports = (req, res) => {
-  res.send(JSON.stringify(process.env, null, 2))
+import { createCanvas } from "canvas"
+
+const SIZE = 500
+
+export default (req, res) => {
+  const canv = createCanvas(SIZE, SIZE)
+  const ctx = canv.getContext("2d")
+
+  ctx.fillStyle = "yellow"
+  ctx.fillRect(0, 0, SIZE, SIZE)
+  ctx.fillStyle = "red"
+  ctx.fillRect(0, 0, SIZE * (3 / 4), SIZE * (3 / 4))
+  ctx.fillStyle = "blue"
+  ctx.fillRect(SIZE * (1 / 4), SIZE * (1 / 4), SIZE, SIZE)
+
+  res.setHeader("Content-Type", "image/png")
+  canv.createPNGStream().pipe(res)
 }
